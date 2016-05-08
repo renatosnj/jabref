5ad159695b533186ec57b6484dfd7108c0cbae45
Wesley Oliveira Souza <wesley.souza@outlook.com>
2016-05-08 19:40:13 -0300

    Implementação de Testes da Classe JabRefPreferences

--- src/test/java/br/com/ufs/ds3/customization/JabRefPreferencesTest.java
+++ src/test/java/br/com/ufs/ds3/customization/JabRefPreferencesTest.java
@@ -1,13 +1,10 @@
 package br.com.ufs.ds3.customization;
 
 import static org.junit.Assert.*;
-
 import java.io.File;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import net.sf.jabref.JabRefException;
 import net.sf.jabref.JabRefPreferences;
 
@@ -28,20 +25,46 @@ public class JabRefPreferencesTest {
     }
 
     @Test
-    public void test() throws JabRefException {
+    public void testImportacaoPreferencias() throws JabRefException {
         File importFile = new File("src/test/resources/net/sf/jabref/customPreferences.xml");
 
         preferencias.importPreferences(importFile.getAbsolutePath());
 
-        String expected = "useProxy";
-        String actual = preferencias.get(JabRefPreferences.PROXY_USE);
-
-        assertEquals(expected, actual);
+        String esperado = "emacsPath";
+        String atual = JabRefPreferences.EMACS_PATH;
+        assertEquals(esperado, atual);
     }
 
     @Test
     public void testPutBoolean() {
         preferencias.putBoolean(JabRefPreferences.BIB_LOC_AS_PRIMARY_DIR, true);
         assertEquals(preferencias.getBoolean(JabRefPreferences.BIB_LOC_AS_PRIMARY_DIR), true);
+    }
+
+    @Test
+    public void testEditor() {
+        assertEquals(JabRefPreferences.EMACS_23, "emacsUseV23InsertString");
+    }
+
+    @Test
+    public void testArquivoImportPreferences() {
+        boolean test = false;
+        File file = new File("jabref.xml");
+        try {
+            JabRefPreferences.getInstance().importPreferences("jabref.xml");
+            test = true;
+        } catch (JabRefException e) {
+            test = false;
+            System.out.println("ERRO!");
+        }
+        assertEquals(true, test);
+    }
+
+    @Test
+    public void testDefaltsPreferences() {
+        String esperado = "Arial";
+        JabRefPreferences.getInstance().defaults.put("FONT_FAMILY", "Arial");
+        String atual = (String) JabRefPreferences.getInstance().defaults.get("FONT_FAMILY");
+        assertEquals(esperado, atual);
     }
 }
